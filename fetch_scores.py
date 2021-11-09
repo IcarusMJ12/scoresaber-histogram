fa0c0b6b0e1ff104342fd210145169291335e418
Igor Kaplounenko <megawidget@gmail.com>
2021-11-09 15:42:11 -0800

    argparse and programmatic y limits

--- fetch_scores.py
+++ fetch_scores.py
@@ -1,32 +1,34 @@
 #!/usr/bin/env python3
 
+from datetime import date
 from urllib import request
 from urllib.error import HTTPError
 import json
-import time
 import pathlib
+import time
 
 
 ENDPOINT = 'https://new.scoresaber.com/api/players/'
 HEADERS = {'User-Agent': 'curl/7.74.0'}
 
 
-def fetch_scores_page(idx):
+def fetch_scores_page(cache, idx):
   req = request.Request(f'{ENDPOINT}{idx}', headers=HEADERS)
   print(f'Requesting page {idx}...')
   with request.urlopen(req) as response:
     body = response.read()
     if len(json.loads(body)['players']) == 0:
       return False
-    with open(f'cache/{idx}.json', 'wb') as f:
+    with open(f'{cache}/{idx}.json', 'wb') as f:
       f.write(body)
   return True
 
 
 def main():
-  pathlib.Path('cache').mkdir(exist_ok=True)
+  cache = str(date.today())
+  pathlib.Path(cache).mkdir(exist_ok=True)
   idx = 1
-  while fetch_scores_page(idx):
+  while fetch_scores_page(cache, idx):
     idx += 1
     time.sleep(0.5)
 
--- make_histogram.py
+++ make_histogram.py
@@ -1,27 +1,62 @@
 #!/usr/bin/env python3
 
+import argparse
+import bisect
 import json
+import math
+import os
+import sys
 
 import matplotlib.pyplot as plt
 
 
-def populate_bins(pp, idx):
+def populate_bins(cache, pp, idx):
   try:
-    with open(f'cache/{idx}.json') as f:
-      pp += [player['pp'] for player in json.load(f)['players'] if player['pp'] > 0]
+    with open(f'{cache}/{idx}.json') as f:
+      pp += [player['pp'] for player in json.load(f)['players']
+             if player['pp'] > 0]
     return True
   except FileNotFoundError:
     return False
 
 
 def main():
-  idx, pp = 1, []
-  while populate_bins(pp, idx):
-    idx += 1
-  pp.sort()
-  bins = int(pp[-1]/100) + 1
+  default_paths = sorted([p for p in next(os.walk('.'))[1]
+                          if not p.startswith('.')])
+  parser = argparse.ArgumentParser(
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument('--scale', '-s', default='linear',
+                      choices=('linear', 'log'),
+                      help="scale to use for the 'y' axis")
+  parser.add_argument('DIR', nargs='*', default=default_paths,
+                      help='directories containing leaderboard dumps')
+  args = parser.parse_args()
 
-  fig = plt.figure()
+  for path in args.DIR:
+    idx, pp = 1, []
+    while populate_bins(path, pp, idx):
+      idx += 1
+    pp.sort()
+    bins = int(math.ceil(pp[-1]/100))
+
+    fig = plt.figure()
+    if args.scale == 'linear':
+      plot_linear(fig, bins, pp)
+    elif args.scale == 'log':
+      plot_logarithmic(fig, bins, pp)
+    plt.xlabel('PP')
+    plt.ylabel('# Players')
+    fig.text(0.72, 0.78, f'{path}\nPP > 0\nbin size = 100')
+  plt.show()
+
+
+def plot_linear(fig, bins, pp):
+  pp_less_100 = bisect.bisect_left(pp, 100)
+  pp_less_200 = bisect.bisect_left(pp, 200)
+  pp_less_300 = bisect.bisect_left(pp, 300)
+  ax1_ylim = math.floor(pp_less_100/1000) * 1000
+  ax2_ylim = math.floor((pp_less_200 - pp_less_100)/1000) * 1000
+  ax3_ylim = math.floor((pp_less_300 - pp_less_200)/1000) * 1000
   fig.subplots_adjust(hspace=0.3)
   ax1 = plt.subplot(6, 1, 1)
   plt.title('PP Size Distribution')
@@ -34,9 +69,9 @@ def main():
   ax2.hist(pp, bins=bins, edgecolor='white', linewidth=1, color='black')
   ax3.hist(pp, bins=bins, edgecolor='white', linewidth=1, color='black')
 
-  ax1.set_ylim(52000, 53000)
-  ax2.set_ylim(18000, 19000)
-  ax3.set_ylim(0, 12000)
+  ax1.set_ylim(ax1_ylim, ax1_ylim + 1000)
+  ax2.set_ylim(ax2_ylim, ax2_ylim + 1000)
+  ax3.set_ylim(0, ax3_ylim + 1000)
 
   ax1.spines.bottom.set_visible(False)
   ax2.spines.top.set_visible(False)
@@ -56,10 +91,12 @@ def main():
   ax2.plot([0, 1], [0, 0], transform=ax2.transAxes, **kwargs)
   ax3.plot([0, 1], [1, 1], transform=ax3.transAxes, **kwargs)
 
-  plt.xlabel('PP')
-  plt.ylabel('# Players')
-  fig.text(0.72, 0.78, '11/04/2021\nPP > 0\nbin size = 100')
-  plt.show()
+
+def plot_logarithmic(fig, bins, pp):
+  plt.title('PP Size Distribution')
+  plt.grid(True)
+  plt.hist(pp, bins=bins, edgecolor='white', linewidth=1, color='black',
+           log=True)
 
 
 if __name__ == '__main__':
